40db499cddc38576bbd2e507284e558fea7c5c9a
Muhammadjon Saidov <99192218+muhammadjonsaidov@users.noreply.github.com>
2026-03-07 18:26:44 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,9 +12,9 @@ export default defineConfig(() => ({
     port: 3000,
     host: true,
     strictPort: true,
-    allowedHosts: ['hyperconfident-carter-sweptback.ngrok-free.dev'],
+    allowedHosts: ['hyperconfident-carter-sweptback.ngrok-free.dev', 'saidovmuhammadjon.me'],
     hmr: {
-      host: 'hyperconfident-carter-sweptback.ngrok-free.dev',
+      host: ['hyperconfident-carter-sweptback.ngrok-free.dev', 'saidovmuhammadjon.me'],
       protocol: 'wss',
       clientPort: 443,
       path: '/frontend/@vite',
